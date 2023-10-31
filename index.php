b22186d04af91e0205845eab3ff078fd3d3d3302
Dev-sajir <144737865+Dev-sajir@users.noreply.github.com>
2023-11-01 00:11:25 +0600

    completed

--- index.php
+++ index.php
@@ -20,13 +20,16 @@ function checkuserstatus()
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-<h2 class="text-center">  <?php checkuserstatus(); ?></h2>
-</div>
+    <h2 class="text-center"> <?php checkuserstatus(); ?></h2>
+    </div>
 </body>
+
 </html>
